4f2439d0745df0cbd0063c070fb45484ae96546a
hcastillo <hcastillo@lsv-tech.com>
2025-08-23 20:04:29 -0500

    corrigiendo fix de jpa cambiando carpeta de escaneo de entities

--- applications/app-service/src/main/java/co/com/pragma/arqclean/MainApplication.java
+++ applications/app-service/src/main/java/co/com/pragma/arqclean/MainApplication.java
@@ -2,14 +2,9 @@ package co.com.pragma.arqclean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {
-        "co.com.pragma.arqclean.entities",  // Donde están tus entidades
-        "co.com.pragma.arqclean.jpa"        // Si tienes entidades en el adapter
-})
 @ConfigurationPropertiesScan
 public class MainApplication {
     public static void main(String[] args) {
--- domain/usecase/src/main/java/co/com/pragma/arqclean/usecase/user/ClientsUseCase.java
+++ domain/usecase/src/main/java/co/com/pragma/arqclean/usecase/user/ClientsUseCase.java
@@ -1,37 +1,4 @@
 package co.com.pragma.arqclean.usecase.user;
 
-import java.util.List;
-import java.util.Optional;
-
-import co.com.pragma.arqclean.model.clients.Clients;
-import co.com.pragma.arqclean.model.clients.gateways.ClientsRepository;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 public class ClientsUseCase {
-    private final ClientsRepository clientsRepository;
-
-    public void saveClient(Clients client){
-        clientsRepository.saveClient(client);
-    }
-
-    public List<Clients> findAllClient(){
-        return clientsRepository.findAllClients();
-    }
-
-    public Optional<Clients> findClientsById(Long id){
-        return clientsRepository.findClientById(id);
-    }
-
-    public void deleteClients(Long id){
-        clientsRepository.deleteClient(id);
-    }
-
-    public boolean existsClientById(Long id){
-        return clientsRepository.existsById(id);
-    }
-
-    public boolean existsClientsByEmail(String email){
-        return clientsRepository.existsByEmail(email);
-    }
 }
